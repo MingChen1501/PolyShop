9e213afb0b80a2204dbf6f6f7e67a5e0556d4f2e
MinhTran <tminh400@gmail.com>
2023-07-05 01:06:05 +0700

    initial commit

---
+++ src/main/java/org/mingchencodelab/polyshop/PolyShopApplication.java
@@ -0,0 +1,13 @@
+package org.mingchencodelab.polyshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolyShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mingchencodelab/polyshop/PolyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mingchencodelab.polyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
